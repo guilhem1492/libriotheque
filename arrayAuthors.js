853fdb5577100f24b63a07a9fe46b5ede2f12b75
Guilhem Martin Saint Léon <guilhem.msl@gmail.com>
2023-10-21 11:30:28 +0200

    arrayAuthors filtrée

--- arrayAuthors.js
+++ arrayAuthors.js
@@ -1,37 +1,40 @@
 import axios from "axios";
-import fs from 'fs';
+import fs from "fs";
 
-      const apiUrl = "https://gutendex.com//books?languages=fr&copyright=false,null";
-      
-      const response = await axios.get(apiUrl);
-      const arrayEbooks = response.data.results
+const apiUrl = "https://gutendex.com//books?languages=fr&copyright=false,null";
 
-      const numberOfPages = Math.ceil(response.data.count / 32)
+const response = await axios.get(apiUrl);
+const arrayEbooks = response.data.results;
 
-        for (let i=2; i<=numberOfPages; i++) {
-          const apiNextPageUrl = "https://gutendex.com/books/?copyright=false%2Cnull&languages=fr&page=" + i
-          const responseNext = await axios.get(apiNextPageUrl);
-          const responseNextResults = responseNext.data.results
-  
-          for (const ebook of responseNextResults) {
-            arrayEbooks.push(ebook)
-          }
-        }
+const numberOfPages = Math.ceil(response.data.count / 32);
 
-      let arrayAuthors = arrayEbooks.map(ebook => ebook.authors).flat();
-      arrayAuthors = [...new Map(arrayAuthors.map(a => [a.name, a])).values()].sort((a, b) => a.name.localeCompare(b.name))
+for (let i = 2; i <= numberOfPages; i++) {
+  const apiNextPageUrl =
+    "https://gutendex.com/books/?copyright=false%2Cnull&languages=fr&page=" + i;
+  const responseNext = await axios.get(apiNextPageUrl);
+  const responseNextResults = responseNext.data.results;
 
-      console.log(arrayAuthors.length);
-      
-      const jsonContent = JSON.stringify(arrayAuthors);
+  for (const ebook of responseNextResults) {
+    arrayEbooks.push(ebook);
+  }
+}
 
-      fs.writeFile("./authors.json", jsonContent, 'utf8', function (err) {
-        if (err) {
-            return console.log(err);
-        }
-    
-        console.log("The file was saved!");
-    }); 
+let arrayAuthors = arrayEbooks.map((ebook) => ebook.authors).flat();
+arrayAuthors = [...new Map(arrayAuthors.map((a) => [a.name, a])).values()]
+  .filter(
+    (a) =>
+      a.name !== "Unknown" && a.name !== "Various" && a.name !== "Anonymous"
+  )
+  .sort((a, b) => a.name.localeCompare(b.name));
 
+console.log(arrayAuthors.length);
 
-    
+const jsonContent = JSON.stringify(arrayAuthors);
+
+fs.writeFile("./authors.json", jsonContent, "utf8", function (err) {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log("The file was saved!");
+});
