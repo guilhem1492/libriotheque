8850673fc73fcc5ffff5d46aef88c3729c5275a0
Guilhem MARTIN SAINT LEON <GMA24035@open-groupe.com>
2023-11-03 16:16:38 +0100

    ajout d'une searchAuthorsBar dans la page indexAuthors

---
+++ src/components/SearchAuthorsBar.jsx
@@ -0,0 +1,54 @@
+import "./components.css";
+import { useState, useContext, useEffect } from "react";
+import { ThemeContext } from "../context/theme.context";
+import { Link } from "react-router-dom";
+import authors from "../../authors.json";
+
+const SearchAuthorsBar = ({ setFilteredAuthors, setFetching }) => {
+  const [authorSearched, setAuthorSearched] = useState("");
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setAuthorSearched(e.target.value)
+  };
+
+  useEffect(() => {
+    
+    if (!authorSearched.length) {
+      setFilteredAuthors(authors)
+    } else {
+      setFilteredAuthors(authors.filter(a => a.name.toLowerCase().includes(authorSearched.toLowerCase().trim())))
+      setFetching(false)
+    }
+    }, [authorSearched]);
+  
+  
+  return (
+    <div className={"search-bar " + theme}>
+      <Link id="libriotheque" to="/libriotheque">
+        Libriothèque
+      </Link>
+
+      <div id="search-container">
+      <Link id="index" to="/libriotheque">
+        Accueil
+      </Link>
+      
+      <input id="search-author-input"
+          type="text"
+          placeholder="Nom de l'auteur(e) ?"
+          onChange={handleChange}
+          value={authorSearched}
+        />
+
+      <button className="theme-btn" onClick={toggleTheme}>
+        {theme === "light" ? "🌙" : "🔆"}
+      </button>
+      </div>
+    </div>
+  );
+};
+
+export default SearchAuthorsBar;
--- src/pages/IndexAuthorsPage.jsx
+++ src/pages/IndexAuthorsPage.jsx
@@ -1,41 +1,24 @@
 import { useState, useEffect } from "react";
 import { Link} from "react-router-dom";
 import BackButton from "../components/BackButton";
-import authors from "../../authors.json";
+import SearchAuthorsBar from "../components/SearchAuthorsBar";
 
 const IndexAuthorsPage = ({setSearchBarDisplay}) => {
     const [filteredAuthors, setFilteredAuthors] = useState([]);
-    const [authorSearched, setAuthorSearched] = useState("");
     const [fetching, setFetching] = useState(true);
-
-    
-
-    const handleChange = (e) => {
-      e.preventDefault();
-      setAuthorSearched(e.target.value)
-    };
 
     useEffect(() => {
       setSearchBarDisplay("notDisplay")
-      if (!authorSearched.length) {
-        setFilteredAuthors(authors)
-      } else {
-        setFilteredAuthors(authors.filter(a => a.name.toLowerCase().includes(authorSearched.toLowerCase().trim())))
-        setFetching(false)
-      }
-      }, [authorSearched]);
+      }, []);
 
   return (
 <div>
-  <BackButton />
-    <h2 id="index-title">Index des auteurs</h2>
 
-        <input id="search-author-input"
-          type="text"
-          placeholder="Nom de l'auteur(e) ?"
-          onChange={handleChange}
-          value={authorSearched}
-        />
+  <SearchAuthorsBar setFilteredAuthors={setFilteredAuthors} setFetching={setFetching} />
+  
+  <BackButton />
+  
+    <h2 id="index-title">Index des auteurs</h2>
 
       {!fetching && !filteredAuthors.length && (
         <p className="no-results">Aucun nom d'auteur(e) ne correspond à votre recherche.</p>
