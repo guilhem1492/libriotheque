64c21b5a325d622e3d014638e1204b1d7c6b0f78
Guilhem Martin Saint Léon <guilhem.msl@gmail.com>
2023-11-06 21:36:43 +0100

    BackButton supprimé sur la Homepage

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -1,14 +1,21 @@
 import "./components.css";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const BackButton = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
-  return (
-    <button id="back-button" onClick={() => navigate(-1)}>
-      ❮❮
-    </button>
-  );
+  //console.log(location.pathname);
+
+  if (location.pathname === "/libriotheque") {
+    return null;
+  } else {
+    return (
+      <button id="back-button" onClick={() => navigate(-1)}>
+        ❮❮
+      </button>
+    );
+  }
 };
 
 export default BackButton;
